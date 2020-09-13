1e93946d13b5cfd7a18b7b7bbe27d9359e19233f
Pedro Papadópolis <pplp93@gmail.com>
2020-09-13 14:34:33 -0300

    feat(tailwind): tailwind installation

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <main>
+	<div class="font-bold">teste</div>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
